7cc6b40f12cbf95fbee39d8279bb7eced40cad39
Dannysia <dsiatkosky@gmail.com>
2020-04-04 15:49:49 -0400

    More Work on Multiple UIs and navigation - End Screen
    Initial Work on Image Handling and Vape/Don't Vape Buttons

--- Proposal1/src/Main.java
+++ Proposal1/src/Main.java
@@ -7,9 +7,12 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 import java.sql.SQLOutput;
+import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 public class Main extends Application {
@@ -34,6 +37,8 @@ public class Main extends Application {
 		Scene infoScreenScene = new Scene(infoScreen, SCREEN_WIDTH, SCREEN_HEIGHT);
 		Parent gameScreen = FXMLLoader.load(getClass().getResource("GameScreen.fxml"));
 		Scene gameScreenScene = new Scene(gameScreen, SCREEN_WIDTH, SCREEN_HEIGHT);
+		Parent endScreen = FXMLLoader.load(getClass().getResource("EndScreen.fxml"));
+		Scene endScreenScene = new Scene(endScreen, SCREEN_WIDTH, SCREEN_HEIGHT);
 
 
 		// Set up Help screen
@@ -47,7 +52,6 @@ public class Main extends Application {
 		backToMainMenuButtonHelpScreen.setOnAction(actionEvent -> {
 			primaryStage.setTitle(titleScreenTitle);
 			primaryStage.setScene(titleScreenScene);
-			primaryStage.show();
 		});
 
 
@@ -64,9 +68,14 @@ public class Main extends Application {
 		backToMainMenuButtonAboutScreen.setOnAction(actionEvent -> {
 			primaryStage.setTitle(titleScreenTitle);
 			primaryStage.setScene(titleScreenScene);
-			primaryStage.show();
 		});
 
+		// End Screen Buttons
+		Button backToMainMenuButtonEndScreen = (Button) endScreen.lookup("#backToMainMenuButton");
+		backToMainMenuButtonEndScreen.setOnAction(actionEvent -> {
+			primaryStage.setTitle(titleScreenTitle);
+			primaryStage.setScene(titleScreenScene);
+		});
 
 
 
@@ -75,9 +84,47 @@ public class Main extends Application {
 		startGame.setOnAction(actionEvent -> {
 			primaryStage.setTitle(gameScreenTitle);
 			primaryStage.setScene(gameScreenScene);
+			/**
+			 * Put code for loading in first situation/pictures here
+			 */
+		});
+
+		/**
+		 * Start Image Testing
+ 		 */
+		Image testImage = new Image("file:Proposal1/src/testImage.jpg");
+		Image testImage2 = new Image("file:Proposal1/src/testImage2.jpg");
+		ImageView testImageView = (ImageView) gameScreen.lookup("#imageTest");
+		/**
+		 * End Image Testing
+		 */
+
+		// Gameplay Buttons
+		Button vapeButton = (Button) gameScreen.lookup("#vapeButton");
+		Button doNotVapeButton = (Button) gameScreen.lookup("#doNotVapeButton");
+
+		vapeButton.setOnAction(actionEvent -> {
+			/**
+			 * Code for handling a user vaping here
+			 */
+			testImageView.setImage(testImage);
+		});
+
+		doNotVapeButton.setOnAction(actionEvent -> {
+			/**
+			 * Code for handling a user not vaping here
+			 */
+			testImageView.setImage(testImage2);
 		});
 
 
+
+
+
+
+
+
+		// Initialize UI
 		primaryStage.setTitle(titleScreenTitle);
 		primaryStage.setScene(titleScreenScene);
 		primaryStage.show();
